298710b965208350384f7489c21a506ed54f4af2
felipe <felipebomfim4161@gmail.com>
2022-11-05 13:01:39 -0300

    Concluded all Basic Algorithm Scripting exercises

---
+++ JavaScript Algorithms and Data Structures/Basic Algorithm Scripting/chunkArrayInGroups.js
@@ -0,0 +1,24 @@
+/*
+Write a function that splits an array (first argument) into groups the length of size (second 
+    argument) and returns them as a two-dimensional array.
+
+*/
+function chunkArrayInGroups(arr, size) {
+    let arr_groups = [];
+    let group = [];
+    for (let i=0; i<arr.length; i++){
+        group.push(arr[i]);
+        if ((i+1)%size == 0){
+            arr_groups.push([...group]);
+            group = [];
+        }     
+    }
+    if (group.length !== 0){
+        console.log("group = "+group);
+
+        arr_groups.push([...group]);
+    }
+    return arr_groups;
+}
+  
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
