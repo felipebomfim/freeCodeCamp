7fb040a836f27b0ed5786236ef046506b0960037
felipe <felipebomfim4161@gmail.com>
2022-11-05 12:23:13 -0300

    Updating de code with de Requirements Statement and fixing a mistake in the reverseString.js code

--- JavaScript Algorithms and Data Structures/Basic Algorithm Scripting/findLongestWordLength.js
+++ JavaScript Algorithms and Data Structures/Basic Algorithm Scripting/findLongestWordLength.js
@@ -1,3 +1,11 @@
+/*
+
+Return the length of the longest word in the provided sentence.
+
+Your response should be a number.
+
+*/
+
 function findLongestWordLength(str) {
   
   let words = str.split(' ');
