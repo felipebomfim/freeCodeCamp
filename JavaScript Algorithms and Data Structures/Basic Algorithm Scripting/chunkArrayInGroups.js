78d82d1597a361c8ce4a9ad8806fcae9f328d5f7
1T BOMFIM CCA-BR <06584154173@ccabr.intraer>
2024-08-07 12:23:27 -0300

    Solved some Basic Algorithm Scripting exercises

--- JavaScript Algorithms and Data Structures/Basic Algorithm Scripting/chunkArrayInGroups.js
+++ JavaScript Algorithms and Data Structures/Basic Algorithm Scripting/chunkArrayInGroups.js
@@ -3,20 +3,30 @@ Write a function that splits an array (first argument) into groups the length of
     argument) and returns them as a two-dimensional array.
 
 */
-function chunkArrayInGroups(arr, size) {
-    let arr_groups = [];
-    let group = [];
-    for (let i=0; i<arr.length; i++){
-        group.push(arr[i]);
-        if ((i+1)%size == 0){
-            arr_groups.push([...group]);
-            group = [];
-        }     
-    }
-    if (group.length !== 0){
-        console.log("group = "+group);
+// function chunkArrayInGroups(arr, size) {
+//     let arr_groups = [];
+//     let group = [];
+//     for (let i=0; i<arr.length; i++){
+//         group.push(arr[i]);
+//         if ((i+1)%size == 0){
+//             arr_groups.push([...group]);
+//             group = [];
+//         }     
+//     }
+//     if (group.length !== 0){
+//         console.log("group = "+group);
 
-        arr_groups.push([...group]);
+//         arr_groups.push([...group]);
+//     }
+//     return arr_groups;
+// }
+
+function chunkArrayInGroups(arr, size){
+    let arr_groups = [];
+    let removed = [];
+    while(arr.length>size){
+        removed = arr.splice(0,size);
+        arr_groups.push(removed);
     }
     return arr_groups;
 }
